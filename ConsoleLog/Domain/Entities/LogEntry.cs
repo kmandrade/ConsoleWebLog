5cd8914d7a846f0bbb3e367f25e471b67090fb62
Kelvin-Acer <Kelvin@DESKTOP-43R6UIP>
2023-11-30 15:58:22 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleLog/Domain/Entities/LogEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLogMVC.Domain.Entities
+{
+    public class LogEntry
+    {
+        public DateTime Timestamp { get; set; }
+        public List<string> Lines { get; set; } = new List<string>();
+    }
+}
---
+++ ConsoleLog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConsoleLog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
